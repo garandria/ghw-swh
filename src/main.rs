f9bebf5037eee052d3cc00989bdd5de6fe214856
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-10-22 10:52:31 -0400

    write url and .github/workflow content to json

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use rayon::prelude::*;
 use std::path::PathBuf;
 use swh_graph::graph::*;
 use swh_graph::NodeType;
-use swh_graph_stdlib;
+use swh_graph_stdlib::*;
+use swh_graph_stdlib::FsTree::Directory;
 use std::result::Result::Ok;
 use anyhow::*;
-
+use std::collections::HashMap;
+use std::*;
+use std::path::Path;
+use serde_json;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -21,7 +25,7 @@ fn main() -> Result<()> {
 	.filter(|&node| props.node_type(node) == NodeType::Origin)
 	.filter(|&node| {
             if let Some(bytes) = props.message(node) {
-		if let Ok(url) = std::str::from_utf8(&bytes) {
+		if let Ok(url) = str::from_utf8(&bytes) {
                     url.starts_with("https://github.com")
 		} else {
                     false
@@ -32,16 +36,35 @@ fn main() -> Result<()> {
 	})
 	.collect();
 
-    origins.into_par_iter().for_each(|ori| {
+    let mut data: HashMap<String, HashMap<String, Vec<String>>> = HashMap::new();
+
+    for ori in origins {
 	if let Ok(Some(snp)) = swh_graph_stdlib::find_latest_snp(&graph, ori) {
 	    if let Ok(Some(hd)) = swh_graph_stdlib::find_head_rev(&graph, snp.0) {
 		if let Ok(Some(rt)) = swh_graph_stdlib::find_root_dir(&graph, hd) {
-		    if let Ok(Some(_)) = swh_graph_stdlib::fs_resolve_path(&graph, rt, ".github/workflows") {
-			println!("{}", std::str::from_utf8(&props.message(ori).unwrap()).unwrap());
+		    if let Ok(Some(ghw)) = swh_graph_stdlib::fs_resolve_path(&graph, rt, ".github/workflows") {
+			let url = str::from_utf8(&props.message(ori).unwrap()).unwrap().to_string();
+			let tree = fs_ls_tree(&graph, ghw).unwrap();
+			let mut ls: Vec<String> = Vec::new();
+			match tree {
+			    Directory(dir) => {
+				for k in dir.keys() {
+				    ls.push(str::from_utf8(&k).unwrap().to_string());
+				    println!("{:?}", k.read());
+				}
+			    },
+			    _ => bail!(""),
+			}
+			let mut h: HashMap<String, Vec<String>> = HashMap::new();
+			h.insert("./github/workflow".to_string(), ls);
+			data.insert(url.to_string(), h);
 		    }
 		}
 	    }
 	}
-    });
+    }
+
+    let path = Path::new("projects.json");
+    fs::write(path, serde_json::to_string_pretty(&data)?)?;
     Ok(())
 }
